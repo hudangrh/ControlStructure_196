2bee534742f9490044d387cb94f25ee57d95551d
hudangrh <hudangrhkuliah@gmail.com>
2026-09-23 16:14:33 +0700

    Prak1

---
+++ Prak1.py
@@ -0,0 +1,13 @@
+grade=int(input("Masukkan grade: "))
+
+if grade >= 90:
+    print("Excellent Performance")
+elif grade >= 80:
+    print("Very Good Performance")
+elif grade >= 70:
+    print("Good Performance")
+elif grade >= 60:
+    print("Average Perfomance")
+else:
+    print("poor Performance")
+    
